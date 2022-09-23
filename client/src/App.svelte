9fd1884f0a1010ca6142f79be013439552e3a3df
Brian Evans <ebrian101@gmail.com>
2022-09-23 13:29:27 +0100

    Switch to NPM, add app spec
    And a little test in Svelte to call the API

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,45 +1,31 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  const FUNCTION_API_URL = "%%FUNCTION_API_URL%%";
+
+  let output, duration
+  async function get(){
+    const startTime = performance.now()
+    const res = await fetch(FUNCTION_API_URL + '/sample/hello?name=Svelte')
+    duration = performance.now() - startTime
+    output = await res.json()
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <h1>Find directors</h1>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <pre>
+    GET {FUNCTION_API_URL}
+  </pre>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <button on:click={get}>GET</button>
+
+  {#if output}
+    <pre>{JSON.stringify(output, null, 2)}</pre>
+    <p>In {duration} milliseconds</p>
+  {/if}
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
