263ea415aadcb035b2a9ea777fa1484ceedcfa26
Brian Evans <ebrian101@gmail.com>
2022-09-24 16:57:12 +0100

    Improve server build method - see desc
    Added more logging. Resolve paths using node:path.resolve instead of import.meta.resolve.
    Also fixed version number in github action workflow and added function url env var to client build.

--- server/src/build.ts
+++ server/src/build.ts
@@ -1,38 +1,42 @@
 /*
 Build source files into one-file-per-Function for deployment, bundled with all dependencies etc.
  */
-import {mkdir, readdir} from 'fs/promises'
-import {resolve,relative,join} from 'node:path'
+import {mkdir, readdir, writeFile} from 'fs/promises'
+import {resolve} from 'node:path'
 import {build} from 'esbuild'
 
-const packagesSrcDir = await getPath('./packages')
-const packagesOutDir = await getPath('../packages')
-const packages = await readdir(packagesSrcDir)
-for (const packageName of packages) {
-    await mkdir(appendPath(packagesOutDir, packageName), {recursive: true})
-    const functions = await readdir(appendPath(packagesSrcDir, packageName))
-    for (const functionName of functions) {
-        console.time("Wrote "+ packageName+'/'+ functionName)
-        const res = await build({
-            bundle: true,
-            outfile: appendPath(packagesOutDir, packageName, functionName + '.js').pathname.slice(1),
-            entryPoints: [appendPath(packagesSrcDir, packageName, functionName , 'index.ts').pathname.slice(1)],
-            format: 'cjs', platform: 'node', minify: true
-        })
-        console.timeEnd("Wrote "+ packageName+'/'+ functionName)
-        res.warnings.forEach(console.log)
-        res.errors.forEach(console.log)
+const packagesSrcDir = resolve(process.argv[2] ?? './packages')
+const packagesOutDir = resolve(process.argv[3] ?? '..')
+
+await buildFunctions(packagesSrcDir,packagesOutDir)
+
+async function buildFunctions(packagesSrcDir, outdir) {
+    const startTime = performance.now()
+    console.log('Source code directory: ', packagesSrcDir)
+    console.log('Output directory: ', outdir)
+    const packages = await readdir(packagesSrcDir)
+    console.log('Found', packages.length, packages.length === 1 ? 'package' : 'packages')
+    const project = {packages: packages.map(p=>({name:p, functions: <{name:string, runtime: string}[]>[]}))}
+    for (const packageName of packages) {
+        await mkdir(resolve(outdir, packageName), {recursive: true})
+        const functions = await readdir(resolve(packagesSrcDir, packageName))
+        for (const functionName of functions) {
+            console.time("Built " + packageName + '/' + functionName)
+            const res = await build({
+                bundle: true,
+                outfile: resolve(outdir, packageName, functionName + '.js'),
+                entryPoints: [resolve(packagesSrcDir, packageName, functionName, 'index.ts')],
+                format: 'cjs', platform: 'node', minify: true
+            })
+            console.timeEnd("Built " + packageName + '/' + functionName)
+            project.packages.find(p=>p.name === packageName)?.functions.push({name: functionName, runtime: 'nodejs:18'})
+            res.warnings.forEach(console.log)
+            res.errors.forEach(console.log)
+        }
     }
-}
+    const duration = performance.now() - startTime
+    console.log('Built in ', Math.round(duration), 'milliseconds')
+    console.log('Run `doctl serverless deploy '+outdir+'` to deploy')
 
-
-async function getPath(relativePath: string){
-    if(import.meta?.resolve) {
-        return await import.meta?.resolve(relativePath).then(f => new URL(f))
-    }else return relativePath
-
-}
-function appendPath(originalUrl: string|URL, ...newSegments: string[]){
-    const newPath = join(originalUrl.toString(), ...newSegments)
-    return new URL(newPath)
+    await writeFile(resolve(outdir, 'project.json'), JSON.stringify(project, null, 2))
 }
